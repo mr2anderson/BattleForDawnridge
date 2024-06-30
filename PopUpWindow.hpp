91379cde11b20e899d8d6ada66200da9e2cc6b05
Chebotarev Nikita <cheb0tarev.nikit0s@gmail.com>
2024-06-30 19:33:55 +0300

    Abstract class PopUpWindow was implemented

---
+++ PopUpWindow.hpp
@@ -0,0 +1,31 @@
+/*
+ *  Conquesta
+ *  Copyright (C) 2023 mr2anderson
+ *
+ *  Conquesta is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Conquesta is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Conquesta.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <SFML/Graphics.hpp>
+#include <cstdint>
+#include <string>
+
+
+#pragma once
+
+
+class PopUpWindow : public sf::Drawable {
+public:
+	virtual std::string click(uint32_t x, uint32_t y) const = 0;
+};
