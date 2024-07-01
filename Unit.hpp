6e3ac3249aaad777b79e6673d8ae70206514ca27
Chebotarev Nikita <cheb0tarev.nikit0s@gmail.com>
2024-07-01 15:12:29 +0300

    Unit was implemented

---
+++ Unit.hpp
@@ -0,0 +1,30 @@
+/*
+ *  Conquesta
+ *  Copyright (C) 2024 mr2anderson
+ *
+ *  Conquesta is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Conquesta is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Conquesta.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#include "GameObject.hpp"
+
+
+class Unit : public GameObject {
+public:
+	Unit(uint32_t x, uint32_t y, uint32_t currentHp, uint32_t maxHp, const Player *playerPtr);
+	bool belongTo(const Player* player) const;
+private:
+	const Player* playerPtr;
+};
