e63e8e43b4f3a1cab61c1d99741fa35ca2944564
Chebotarev Nikita <cheb0tarev.nikit0s@gmail.com>
2024-07-01 09:42:35 +0300

    Menu was improved

--- MenuScreen.cpp
+++ MenuScreen.cpp
@@ -29,14 +29,21 @@ int32_t MenuScreen::run(sf::RenderWindow& window) {
 int32_t MenuScreen::getConnection(sf::RenderWindow& window) {
 	MusicStorage::get()->get("menu")->play();
 
-	Label license(10, window.getSize().y - 10 - 250, 500, 250, L"Conquesta\nCopyright (C) 2023 mr2anderson\n\nConquesta is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nhe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\nConquesta is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with Conquesta.  If not, see <http://www.gnu.org/licenses/>.", 14);
+	Label license(10, window.getSize().y - 10 - 315, 500, 315, L"Conquesta\nCopyright (C) 2023 mr2anderson\n\nConquesta is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nhe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\nConquesta is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with Conquesta.  If not, see <http://www.gnu.org/licenses/>.\n\nЕсли Вам понравился этот проект, то Вы можете поддержать\nразработчиков, отправив небольшую сумму на этот BTC кошелек:\nbc1ql9np8zlq487lmxv60v9ukrp83gtjthvxupwexs", 14);
 	Button start2on1pc(10, 10, 400, 60, L"В бой (2 игрока 1 ПК)", 24);
 	Button exit(10, 80, 400, 60, L"Завершить", 24);
 	sf::Sprite sprite;
+	sf::Text title;
+
+	title.setFont(*FontStorage::get()->get("1"));
+	title.setString("CONQUESTA");
+	title.setCharacterSize(60);
+	title.setFillColor(sf::Color::White);
+	title.setPosition(window.getSize().x - 40 - title.getLocalBounds().width, window.getSize().y - 40 - title.getLocalBounds().height);
 	
 	sf::Clock clock;
 	sprite.setTexture(*TextureStorage::get()->get("logotype0"));
-	sprite.setPosition(window.getSize().x - 10 - sprite.getLocalBounds().width, window.getSize().y - 10 - sprite.getLocalBounds().height);
+	sprite.setPosition(sf::Vector2f(title.getPosition().x, title.getPosition().y - sprite.getLocalBounds().height));
 
 	sf::Event event{};
 
@@ -68,6 +75,7 @@ int32_t MenuScreen::getConnection(sf::RenderWindow& window) {
 		window.draw(license);
 		window.draw(start2on1pc);
 		window.draw(exit);
+		window.draw(title);
 		window.draw(sprite);
 		window.display();
 
