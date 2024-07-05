3167c5451ae4260296b7c3df25ba46ee14476d3c
Chebotarev Nikita <cheb0tarev.nikit0s@gmail.com>
2024-07-05 22:19:15 +0300

    Bug in wall was fixed

--- Wall.cpp
+++ Wall.cpp
@@ -87,14 +87,14 @@ Event Wall::getSelectionW() {
 
 	std::vector<SelectionWComponent> components;
 	components.emplace_back("exit_icon", L"Покинуть", true, true, Event());
-	components.emplace_back("wall" + std::to_string(this->getCurrentLevel() + 1),
+	components.emplace_back("wall" + std::to_string(this->getCurrentLevel()),
 		this->getDescription() + L"\n"
 		+ this->getReadableHpInfo(), false, false, Event());
 
 	if (this->getCurrentLevel() != TOTAL_LEVELS) {
 		Event gameEventUpgrade;
 		gameEventUpgrade.tryToUpgrade.emplace_back(this, this->getUpgradeCost());
-		components.emplace_back("upgrade_icon",
+		components.emplace_back("wall" + std::to_string(this->getCurrentLevel() + 1),
 			L"Улучшить стену за " + this->getUpgradeCost().getReadableInfo() + L"\n"
 			"Улучшение увеличит защиту с " + std::to_wstring(LEVEL_HP[this->getCurrentLevel() - 1]) + L" до " + std::to_wstring(LEVEL_HP[this->getCurrentLevel()]) +
 			L" и скорость ремонта с " + std::to_wstring(this->getRegenerationSpeed()) + L" до " + std::to_wstring(GET_REGENERATION_SPEED(this->getCurrentLevel())) + L".", true, false, gameEventUpgrade);
