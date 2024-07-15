fbb707d72bd167b12bbf645438546c86d3a5f252
Chebotarev Nikita <cheb0tarev.nikit0s@gmail.com>
2024-07-15 13:34:37 +0300

    Added guide in menu

--- Menu.cpp
+++ Menu.cpp
@@ -89,8 +89,13 @@ void Menu::init(uint32_t windowW, uint32_t windowH) {
     this->windowH = windowH;
     this->element = nullptr;
 
+
+
     Events clickEvent;
     clickEvent.add(std::make_shared<PlaySoundEvent>("click"));
+
+
+
 
     Events startGameOnRidgeEvent = clickEvent;
     startGameOnRidgeEvent.add(std::make_shared<StartGameEvent>("ridge"));
@@ -112,11 +117,84 @@ void Menu::init(uint32_t windowW, uint32_t windowH) {
         true,
         createStartGameOnRidgeVerifyWindow
     );
+
     std::shared_ptr<HorizontalSelectionWindow> chooseLevelWindow = std::make_shared<HorizontalSelectionWindow>(chooseLevelWindowComponents, Maps::THUMBNAIL_SIZE);
     Events createChooseLevelWindowEvent = clickEvent;
     createChooseLevelWindowEvent.add(std::make_shared<CreateEEvent>(chooseLevelWindow));
-
 	this->buttons.emplace_back(std::make_shared<Label>(10, 10, 400, 60, *Texts::get()->get("start_game_local")), createChooseLevelWindowEvent);
+
+
+
+
+    std::vector<HorizontalSelectionWindowComponent> educationWindowComponents;
+    educationWindowComponents.emplace_back(
+        "exit_icon",
+        *Texts::get()->get("leave"),
+        true,
+        clickEvent
+    );
+    educationWindowComponents.emplace_back(
+        "food_icon",
+        *Texts::get()->get("guide_food_description"),
+        false,
+        Events()
+    );
+    educationWindowComponents.emplace_back(
+        "wood_icon",
+        *Texts::get()->get("guide_wood_description"),
+        false,
+        Events()
+    );
+    educationWindowComponents.emplace_back(
+        "stone_icon",
+        *Texts::get()->get("guide_stone_description"),
+        false,
+        Events()
+    );
+    educationWindowComponents.emplace_back(
+        "iron_icon",
+        *Texts::get()->get("guide_iron_description"),
+        false,
+        Events()
+    );
+    educationWindowComponents.emplace_back(
+        "gold_icon",
+        *Texts::get()->get("guide_gold_description"),
+        false,
+        Events()
+    );
+    educationWindowComponents.emplace_back(
+        "road",
+        *Texts::get()->get("guide_road_system_description"),
+        false,
+        Events()
+    );
+    educationWindowComponents.emplace_back(
+        "shield_icon",
+        *Texts::get()->get("guide_damage_mods_description"),
+        false,
+        Events()
+    );
+    educationWindowComponents.emplace_back(
+        "warehouse_food",
+        *Texts::get()->get("guide_storage_system_description"),
+        false,
+        Events()
+    );
+    educationWindowComponents.emplace_back(
+        "wall2",
+        *Texts::get()->get("guide_wall_system_description"),
+        false,
+        Events()
+    );
+
+    std::shared_ptr<HorizontalSelectionWindow> educationWindow = std::make_shared<HorizontalSelectionWindow>(educationWindowComponents);
+    Events createEducationWindowEvent = clickEvent;
+    createEducationWindowEvent.add(std::make_shared<CreateEEvent>(educationWindow));
+    this->buttons.emplace_back(std::make_shared<Label>(10, 80, 400, 60, *Texts::get()->get("education")), createEducationWindowEvent);
+
+
+
 
     Events chooseLanguageEnglishEvent;
     chooseLanguageEnglishEvent.add(std::make_shared<ChooseLanguageEvent>("en.txt"));
@@ -143,26 +221,38 @@ void Menu::init(uint32_t windowW, uint32_t windowH) {
         true,
         chooseLanguageRussianEvent
     );
+
     std::shared_ptr<HorizontalSelectionWindow> chooseLanguageWindow = std::make_shared<HorizontalSelectionWindow>(chooseLanguageWindowComponents);
     Events createChooseLanguageWindowEvent = clickEvent;
     createChooseLanguageWindowEvent.add(std::make_shared<CreateEEvent>(chooseLanguageWindow));
-    
-    this->buttons.emplace_back(std::make_shared<Label>(10, 80, 400, 60, *Texts::get()->get("language")), createChooseLanguageWindowEvent);
+    this->buttons.emplace_back(std::make_shared<Label>(10, 150, 400, 60, *Texts::get()->get("language")), createChooseLanguageWindowEvent);
+
+
+
 
     std::shared_ptr<WindowButtonImage> supportWindow = std::make_shared<WindowButtonImage>(*Texts::get()->get("support"), *Texts::get()->get("close"), "btc", clickEvent, 400, 300);
     Events supportEvent = clickEvent;
     supportEvent.add(std::make_shared<CreateEEvent>(supportWindow));
-    this->buttons.emplace_back(std::make_shared<Label>(10, 150, 400, 60,  *Texts::get()->get("show_support")), supportEvent);
+    this->buttons.emplace_back(std::make_shared<Label>(10, 220, 400, 60,  *Texts::get()->get("show_support")), supportEvent);
+
+
+
 
     std::shared_ptr<WindowButton> creditsWindow = std::make_shared<WindowButton>(*Texts::get()->get("credits"), *Texts::get()->get("close"), clickEvent);
     Events creditsEvent = clickEvent;
     creditsEvent.add(std::make_shared<CreateEEvent>(creditsWindow));
-    this->buttons.emplace_back(std::make_shared<Label>(10, 220, 400, 60, *Texts::get()->get("show_credits")), creditsEvent);
+    this->buttons.emplace_back(std::make_shared<Label>(10, 290, 400, 60, *Texts::get()->get("show_credits")), creditsEvent);
+
+
+
 
     std::shared_ptr<WindowButton> licenseWindow = std::make_shared<WindowButton>(*Texts::get()->get("license"), *Texts::get()->get("close"), clickEvent, 600, 400);
     Events licenseEvent = clickEvent;
     licenseEvent.add(std::make_shared<CreateEEvent>(licenseWindow));
-    this->buttons.emplace_back(std::make_shared<Label>(10, 290, 400, 60, *Texts::get()->get("show_license")), licenseEvent);
+    this->buttons.emplace_back(std::make_shared<Label>(10, 360, 400, 60, *Texts::get()->get("show_license")), licenseEvent);
+
+
+
 
     Events exitEvent;
     exitEvent.add(std::make_shared<CloseMenuEvent>());
@@ -170,8 +260,7 @@ void Menu::init(uint32_t windowW, uint32_t windowH) {
     std::shared_ptr<WindowTwoButtons> confirmExitWindow = std::make_shared<WindowTwoButtons>(*Texts::get()->get("confirm_exit"), *Texts::get()->get("yes"), *Texts::get()->get("no"), exitEvent, clickEvent);
     Events createConfirmExitWindowEvent = clickEvent;
     createConfirmExitWindowEvent.add(std::make_shared<CreateEEvent>(confirmExitWindow));
-
-	this->buttons.emplace_back(std::make_shared<Label>(10, 360, 400, 60, *Texts::get()->get("exit")), createConfirmExitWindowEvent);
+	this->buttons.emplace_back(std::make_shared<Label>(10, 430, 400, 60, *Texts::get()->get("exit")), createConfirmExitWindowEvent);
 
     this->background.setTexture(*Textures::get()->get("menu"));
     this->background.setPosition(windowW - this->background.getLocalBounds().width, windowH - this->background.getLocalBounds().height);
